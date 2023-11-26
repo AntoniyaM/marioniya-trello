74e2b60819f12592a27dcf0ec256b2c3fa0ea32f
Antoniya Mincheva <a.mincheva@trio-group.de>
2023-11-26 17:53:29 +0100

    🔧 Added navbar.

---
+++ src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="navbar">
+    <BaseButton icon="pi pi-user" :label="user ? 'Log out' : 'Log in'" />
+  </div>
+</template>
+
+<script setup>
+import { useFirebaseAuth } from 'vuefire';
+
+const user = useFirebaseAuth().currentUser;
+</script>
