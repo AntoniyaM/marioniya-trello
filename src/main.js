b6a2ffcb8639b453e21f04cdae3b161fd5c5144b
Antoniya Mincheva <a.mincheva@trio-group.de>
2023-11-26 17:52:33 +0100

    🛠 Removed default components, added PrimeVue.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-import './assets/main.css';
+import '@/assets/scss/app.scss';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import { VueFire } from 'vuefire';
+// Firebase.
+import { VueFire, VueFireAuth } from 'vuefire';
 import { firebaseApp } from '@/firebase';
+// PrimeVue.
+import PrimeVue from 'primevue/config';
+import Button from 'primevue/button';
 
 import App from './App.vue';
 import router from './router';
@@ -14,7 +18,11 @@ app.use(createPinia());
 app.use(router);
 app.use(VueFire, {
   firebaseApp,
-  modules: [],
+  modules: [VueFireAuth()],
 });
+app.use(PrimeVue, { ripple: true });
+
+// Component registration.
+app.component('BaseButton', Button);
 
 app.mount('#app');
---
+++ src/views/Board.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="board">
+    <h1>Mario & Antoniya's Trello Board</h1>
+  </div>
+</template>
+
+<script setup></script>
